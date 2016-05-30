89bcdf516cdd617a0fbf4a1f2bf199d48f461f1e
jsg1504 <jsg1504@gmail.com>
2016-05-30 22:22:40 +0900

    gmail send complete. but the mail is not allowed

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,7 +4,18 @@ class EmailController < ApplicationController
 
   def email_send
     @email = params[:email]
+    @title = params[:title]
     @content = params[:content]
+
+    gmail = Gmail.connect("jsg1504@gmail.com","google1504")
+    gmail.deliver do
+      to email
+      subject title
+      text_part do
+        body content
+      end
+    end
+    gmail.logout
   end
 
 end
